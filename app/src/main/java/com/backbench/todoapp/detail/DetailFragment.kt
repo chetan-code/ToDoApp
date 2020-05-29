eb2a5fc8014c9f5bc68573d2bec1c8bfe684073d
chetan-code <sharma.chetan1998@gmail.com>
2020-05-29 21:35:21 +0530

    Some edits

--- app/src/main/java/com/backbench/todoapp/detail/DetailFragment.kt
+++ app/src/main/java/com/backbench/todoapp/detail/DetailFragment.kt
@@ -1,11 +1,13 @@
 package com.backbench.todoapp.detail
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.backbench.todoapp.R
 import com.backbench.todoapp.database.TodoDatabase
@@ -17,10 +19,12 @@ import com.backbench.todoapp.databinding.FragmentDetailBinding
  */
 class DetailFragment : Fragment() {
 
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View? {
 
         val binding : FragmentDetailBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_detail,container,false)
+
 
         val application = requireNotNull(this.activity).application
         val dataSource = TodoDatabase.getInstance(application).todoDatabaseDao
